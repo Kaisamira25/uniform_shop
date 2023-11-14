06a00a4ef2ed01b3b88a336e0ced2adae6071a45
le ngoc anh kiet <leanhkiet249@gmail.com>
2023-11-14 16:36:36 +0700

    #5 - fix login,register from class component to func component

--- src/components/_Regform.jsx
+++ src/components/_Regform.jsx
@@ -1,92 +1,95 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
-class REGForm extends Component {
-  constructor() {
-    super();
-    this.state = {
-      email: '',
-      username: '',
-      phoneNumber: '',
-      password: '',
-      confirmPassword: '',
-    };
-  }
+export default function REGForm() {
+  const [formData, setFormData] = useState({
+    email: '',
+    username: '',
+    phoneNumber: '',
+    password: '',
+    confirmPassword: '',
+  });
 
-  handleInputChange = (e) => {
+  const handleInputChange = (e) => {
     const { name, value, type, checked } = e.target;
 
-    if (type === 'checkbox') {
-      this.setState({ [name]: checked });
-    } else {
-      this.setState({ [name]: value });
-    }
-  }
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: type === 'hide' ? checked : value,
+    }));
+  };
 
-  handleSubmit = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
-  }
 
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <div className="mb-4">
-          <label htmlFor="email" className="block text-sm font-medium text-gray-700">Email</label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            value={this.state.email}
-            onChange={this.handleInputChange}
-            className="w-full p-2 border rounded border-gray-300"
-          />
-        </div>
-        <div className="mb-4">
-          <label htmlFor="username" className="block text-sm font-medium text-gray-700">Tên đăng nhập</label>
-          <input
-            type="text"
-            id="username"
-            name="username"
-            value={this.state.username}
-            onChange={this.handleInputChange}
-            className="w-full p-2 border rounded border-gray-300"
-          />
-        </div>
-        <div className="mb-4">
-          <label htmlFor="phoneNumber" className="block text-sm font-medium text-gray-700">Số điện thoại</label>
-          <input
-            type="tel"
-            id="phoneNumber"
-            name="phoneNumber"
-            value={this.state.phoneNumber}
-            onChange={this.handleInputChange}
-            className="w-full p-2 border rounded border-gray-300"
-          />
-        </div>
-        <div className="mb-4">
-          <label htmlFor="password" className="block text-sm font-medium text-gray-700">Mật khẩu</label>
-          <input
-            type="password"
-            id="password"
-            name="password"
-            value={this.state.password}
-            onChange={this.handleInputChange}
-            className="w-full p-2 border rounded border-gray-300"
-          />
-        </div>
-        <div className="mb-4">
-          <label htmlFor="confirmPassword" className="block text-sm font-medium text-gray-700">Lặp lại mật khẩu</label>
-          <input
-            type="password"
-            id="confirmPassword"
-            name="confirmPassword"
-            value={this.state.confirmPassword}
-            onChange={this.handleInputChange}
-            className="w-full p-2 border rounded border-gray-300"
-          />
-        </div>
-      </form>
-    );
-  }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="mb-4">
+        <label htmlFor="username" className="block text-sm font-medium text-gray-700">
+          Tên đăng nhập
+        </label>
+        <input
+          type="text"
+          id="username"
+          name="username"
+          value={formData.username}
+          onChange={handleInputChange}
+          className="w-full p-2 border rounded border-gray-300"
+        />
+      </div>
+      <div className="mb-4">
+        <label htmlFor="email" className="block text-sm font-medium text-gray-700">
+          Email
+        </label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          value={formData.email}
+          onChange={handleInputChange}
+          className="w-full p-2 border rounded border-gray-300"
+        />
+      </div>
+      <div className="mb-4">
+        <label htmlFor="phoneNumber" className="block text-sm font-medium text-gray-700">
+          Số điện thoại
+        </label>
+        <input
+          type="tel"
+          id="phoneNumber"
+          name="phoneNumber"
+          value={formData.phoneNumber}
+          onChange={handleInputChange}
+          className="w-full p-2 border rounded border-gray-300"
+        />
+      </div>
+      <div className="mb-4">
+        <label htmlFor="password" className="block text-sm font-medium text-gray-700">
+          Mật khẩu
+        </label>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          value={formData.password}
+          onChange={handleInputChange}
+          className="w-full p-2 border rounded border-gray-300"
+        />
+      </div>
+      <div className="mb-4">
+        <label htmlFor="confirmPassword" className="block text-sm font-medium text-gray-700">
+          Lặp lại mật khẩu
+        </label>
+        <input
+          type="password"
+          id="confirmPassword"
+          name="confirmPassword"
+          value={formData.confirmPassword}
+          onChange={handleInputChange}
+          className="w-full p-2 border rounded border-gray-300"
+        />
+      </div>
+    </form>
+  );
 }
-
-export default REGForm;
